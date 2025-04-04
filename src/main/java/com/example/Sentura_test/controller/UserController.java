d0fb9c5c665c09fda63dbb0acf21651d10fb0df8
Amodhx <amxdhnanditha@gmail.com>
2025-04-04 09:45:49 +0530

    created save user and get user

--- src/main/java/com/example/Sentura_test/controller/UserController.java
+++ src/main/java/com/example/Sentura_test/controller/UserController.java
@@ -2,6 +2,10 @@ package com.example.Sentura_test.controller;
 
 
 import com.example.Sentura_test.dto.UserDTO;
+import com.example.Sentura_test.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,17 +13,21 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
     @GetMapping("/{user_id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable String user_id,
                                            @RequestParam(required = false) Boolean trashed){
-
+        UserDTO user = userService.getUser(user_id,trashed);
+        return new ResponseEntity<>(user,HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<UserDTO> saveUser(@RequestBody UserDTO userDTO){
-
+        UserDTO user = userService.saveUser(userDTO);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @PutMapping("/{user_id}")
--- src/main/java/com/example/Sentura_test/service/UserService.java
+++ src/main/java/com/example/Sentura_test/service/UserService.java
@@ -1,9 +1,87 @@
 package com.example.Sentura_test.service;
 
+import com.example.Sentura_test.dto.UserDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private OkHttpClient client;
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @Value("${weavy.api.url}")
+    private String weavyApiUrl;
+
+    @Value("${weavy.api.key}")
+    private String weavyApiToken;
+
+
+    public UserDTO saveUser(UserDTO userDTO) {
+        String endpoint = weavyApiUrl + "/api/users";
+        try {
+
+            String jsonBody = objectMapper.writeValueAsString(userDTO);
+
+            RequestBody body = okhttp3.RequestBody.create(
+                    jsonBody,
+                    MediaType.parse("application/json")
+            );
+
+            Request request = new Request.Builder()
+                    .url(endpoint)
+                    .post(body)
+                    .addHeader("Authorization", "Bearer " + weavyApiToken)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                if (response.isSuccessful() && response.body() != null) {
+                    String responseBody = response.body().string();
+                    return objectMapper.readValue(responseBody, UserDTO.class);
+                } else {
+                    throw new RuntimeException("Failed to save user: " + response);
+                }
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting user to JSON", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Error making HTTP request to Weavy API", e);
+        }
+    }
+    public UserDTO getUser(String userId, Boolean trashed) {
+        String endpoint = weavyApiUrl + "/api/users/" + userId;
+        if (trashed != null) {
+            endpoint += "?trashed=" + trashed;
+        }
+
+        Request request = new Request.Builder()
+                .url(endpoint)
+                .get()
+                .addHeader("Authorization", "Bearer " + weavyApiToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful() && response.body() != null) {
+                String responseBody = response.body().string();
+                return objectMapper.readValue(responseBody, UserDTO.class);
+            } else {
+                throw new RuntimeException("Cant get User " + response.message());
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("response to UserDTO", e);
+        } catch (IOException e) {
+            throw new RuntimeException("request to Weavy API", e);
+        }
+    }
 }
