ae3657943b2d02f2d3b59f6ccc3a704a89cefdbf
Amodhx <amxdhnanditha@gmail.com>
2025-04-04 09:05:55 +0530

    added dependency to validation and okHttp

--- src/main/java/com/example/Sentura_test/SenturaTestApplication.java
+++ src/main/java/com/example/Sentura_test/SenturaTestApplication.java
@@ -1,13 +1,20 @@
 package com.example.Sentura_test;
 
+import okhttp3.OkHttpClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SenturaTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SenturaTestApplication.class, args);
+	}
+
+	@Bean
+	OkHttpClient okHttpClient(){
+		return new OkHttpClient();
 	}
 
 }
