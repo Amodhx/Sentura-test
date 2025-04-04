dc3b679bc145f7f003a558acfcb7fec622b72500
Amodhx <amxdhnanditha@gmail.com>
2025-04-04 08:42:05 +0530

    Initial commit

---
+++ src/main/java/com/example/Sentura_test/SenturaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sentura_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenturaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenturaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sentura_test/SenturaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sentura_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenturaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
